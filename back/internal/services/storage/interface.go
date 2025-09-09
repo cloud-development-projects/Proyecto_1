e307503b28e5bb5f11f2301b1316599778aa83c6
Jheisson Orlando Cabezas Vera <jheissoncabezas@gmail.com>
2025-09-09 11:32:36 -0500

    Se ajustan cambios de Storage

--- back/internal/services/storage/interface.go
+++ back/internal/services/storage/interface.go
@@ -4,10 +4,8 @@ import (
 	"mime/multipart"
 )
 
-// Storage defines a generic interface for saving, retrieving, and deleting files.
 type Storage interface {
-    SaveFile(file multipart.File, filename string) (string, error) // returns public URL or path
-    DeleteFile(filename string) error
-    GetFileURL(filename string) string
-    GetProcessedFilePath(videoID string) (string, error)
+	SaveFile(file multipart.File, destPath string) error
+	DeleteFile(path string) error
+	GetProcessedFilePath(videoID string) (string, error)
 }
